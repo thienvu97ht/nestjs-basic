ddbdbdd9289fc7888cd0ced05d177eba05c5c2fd
thienvu97ht <thien.vu97ht123@gmail.com>
2023-05-22 16:38:40 +0700

    #21. Create Schema (Model)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get, Render } from "@nestjs/common";
 import { AppService } from "./app.service";
 import { ConfigService } from "@nestjs/config";
-
+import { ApiTags } from "@nestjs/swagger";
+@ApiTags("Home")
 @Controller()
 export class AppController {
   constructor(
--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,6 @@ async function bootstrap() {
     .setTitle("Learn NestJS")
     .setDescription("The cats API description")
     .setVersion("1.0")
-    .addTag("cats")
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
