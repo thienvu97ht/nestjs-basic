710e2c4d3f673f46147475c3bc8bc2a0e1cc7e06
DESKTOP-8RSPN1O\DuongVu <thien.vu97ht123@gmail.com>
2023-06-22 19:35:31 +0700

    #54. Bài tập Create Company

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { CompaniesService } from './companies.service';
-import { CreateCompanyDto } from './dto/create-company.dto';
-import { UpdateCompanyDto } from './dto/update-company.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from "@nestjs/common";
+import { CompaniesService } from "./companies.service";
+import { CreateCompanyDto } from "./dto/create-company.dto";
+import { UpdateCompanyDto } from "./dto/update-company.dto";
+import { ApiTags, ApiBearerAuth } from "@nestjs/swagger";
 
-@Controller('companies')
+@ApiTags("Company")
+@ApiBearerAuth()
+@Controller("companies")
 export class CompaniesController {
   constructor(private readonly companiesService: CompaniesService) {}
 
@@ -17,18 +28,18 @@ export class CompaniesController {
     return this.companiesService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     return this.companiesService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCompanyDto: UpdateCompanyDto) {
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateCompanyDto: UpdateCompanyDto) {
     return this.companiesService.update(+id, updateCompanyDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.companiesService.remove(+id);
   }
 }
